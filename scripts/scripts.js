34a738e9aa52b0090c83e0ed5dd2ba1280f0eaf1
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-03-05 14:49:50 +0200

    Add logs to fetch specific data from Songkick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,10 @@
 const KOLIBRE_ID = 9321674;
 
-console.log('SOMETHING HERE');
-
 fetch('https://api.songkick.com/api/3.0/artists/190740/calendar.json?apikey=hynKk019PMWTRsXP')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
+    console.log(data.resultsPage.results.event[0].start.date);
+    console.log(data.resultsPage.results.event[0].venue.displayName);
   });
